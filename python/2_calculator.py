7bffa2dbd008c88da26b49371a44a813b245b240
Jie Yu <jyu238@myseneca.ca>
2024-11-24 19:38:04 -0500

    simple calculate and display with format

---
+++ python/2_calculator.py
@@ -0,0 +1,33 @@
+#########       int
+
+#x = input("What's x? ")
+#y = input("What's y? ")
+
+#z = x + y
+
+#print(z)
+
+#make code more readable and keep them simple is important
+#x = int(input("What's x? "))
+#y = int(input("What's y? "))
+
+#z = int(x) + int(y)
+
+#print(x + y)
+
+#the code is too complex and think too much
+#print(int(input("What's x? "))+int(input("What's y? ")))
+
+#######float
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+#round(number[, ndigits]) square bracket here is an option
+z = round(x + y)
+#format string
+print(f"{z:,}")
+
+m = x / y
+print(f"{m:.2f}")
+
+
