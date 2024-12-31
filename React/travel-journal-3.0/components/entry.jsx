88fc7404201c2de4e95ce27e2403b89104ed8c3f
Jie Yu <jieyu1233@gmail.com>
2024-12-31 17:51:36 -0500

    more concise way to pass {...entry} down, it equals that img={entry.img} ...

--- React/travel-journal-3.0/components/entry.jsx
+++ React/travel-journal-3.0/components/entry.jsx
@@ -5,11 +5,7 @@ export default function Entry(props) {
   return (
     <article className="journal-entry">
       <div className="main-image-container">
-        <img
-          className="main-image"
-          src={props.entry.img?.src}
-          alt={props.entry.img?.alt}
-        />
+        <img className="main-image" src={props.img?.src} alt={props.img?.alt} />
       </div>
       <div className="info-container">
         <img
@@ -17,11 +13,11 @@ export default function Entry(props) {
           src="./images/marker.png"
           alt="marker icon"
         />
-        <span className="country">{props.entry.country}</span>
-        <a href={props.entry.googleMapsLink}>View on the Google Map</a>
-        <h2 className="entry-title">{props.entry.title}</h2>
-        <p className="trip-dates">{props.entry.dates}</p>
-        <p className="entry-text">{props.entry.text}</p>
+        <span className="country">{props.country}</span>
+        <a href={props.googleMapsLink}>View on the Google Map</a>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
     </article>
   );
