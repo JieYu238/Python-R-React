e515b567095d0e411cfe0ae91af7747f47a9e224
Jie Yu <jieyu1233@gmail.com>
2024-12-31 17:03:51 -0500

    create jokesData.js then using .map() method, the code becomes more clear!

--- React/props-practice-joke/app.jsx
+++ React/props-practice-joke/app.jsx
@@ -1,37 +1,10 @@
 import React from "react";
 import Joke from "./components/joke";
+import jokeData from "./jokesData";
 
 export default function App() {
-  return (
-    <main>
-      <Joke
-        punchLine="I can't wait to see her face light up when she opens it."
-        upvotes={10}
-        isPun={true}
-        comments={[
-          {
-            author: "",
-            text: "",
-            title: "",
-          },
-        ]}
-      />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchLine="He just ransomware!"
-      />
-      <Joke
-        setup="Why don't pirates travel on mountain roads?"
-        punchLine="Scurvy."
-      />
-      <Joke
-        setup="Why do bees stay in the hive in the winter?"
-        punchLine="Swarm."
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchLine="I don't know, but the flag is a big plus!"
-      />
-    </main>
-  );
+  const jokeElements = jokeData.map((joke) => {
+    return <Joke setup={joke.setup} punchLine={joke.punchline} />;
+  });
+  return <main>{jokeElements}</main>;
 }
