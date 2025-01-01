8b2f0376e846402380bb1e0766ef4b1ea4dba78f
Jie Yu <jieyu1233@gmail.com>
2024-12-31 21:51:22 -0500

    create static parts

---
+++ React/chef-claude/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/header";
+import Main from "./components/main";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
---
+++ React/chef-claude/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="./images/chef-logo.png" alt="chef logo" />
+      <span>Chef Claude</span>
+    </header>
+  );
+}
