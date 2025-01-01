9f1f1c14f3108e52ce1c2f3d1ea76e020bb5d4c4
Jie Yu <jieyu1233@gmail.com>
2025-01-01 17:25:50 -0500

    add ingredients

--- React/chef-claude/components/main.jsx
+++ React/chef-claude/components/main.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState([
+    "Chicken",
+    "Oregano",
+    "Tomatoes",
+  ]);
+  const IngredientsListItems = ingredients.map((item) => {
+    return <li key={item}>{item}</li>;
+  });
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((preIngredients) => [...preIngredients, newIngredient]);
+  }
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul className="ingredientsListItems">{IngredientsListItems}</ul>
     </main>
   );
 }
