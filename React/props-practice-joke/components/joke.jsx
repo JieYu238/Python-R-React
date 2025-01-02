31f582de23ee3c7e28723d6965f8d0fe5282f5f3
Jie Yu <jieyu1233@gmail.com>
2025-01-01 20:46:59 -0500

    conditional rendering, ternary condition

--- React/props-practice-joke/components/joke.jsx
+++ React/props-practice-joke/components/joke.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 export default function Joke(props) {
+  const [isShown, setIsShown] = React.useState(false);
+  function toggleShown() {
+    setIsShown((prev) => !prev);
+  }
   return (
     <div className="joke-div">
-      {props.setup && <p className="setup">Setup: {props.setup}</p>}
-      <p className="punchline">Punchline: {props.punchLine}</p>
+      {props.setup && <p className="setup">{props.setup}</p>}
+      {isShown ? <p className="punchline">{props.punchLine}</p> : null}
+      <button onClick={toggleShown}>
+        {isShown ? "Hide" : "Show"} punchline
+      </button>
+
       <hr />
     </div>
   );
