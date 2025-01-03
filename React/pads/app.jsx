1c4d5925986d7219d985a09a172fc061f258da24
Jie Yu <jieyu1233@gmail.com>
2025-01-03 12:10:47 -0500

    more react way

--- React/pads/app.jsx
+++ React/pads/app.jsx
@@ -3,23 +3,27 @@ import padsData from "./pads";
 import Pad from "./pad";
 export default function App() {
   const [pads, setPads] = React.useState(padsData);
-  function turnAllPadsOff() {
+  function toggle(id) {
     setPads((prevPads) =>
-      padsData.map((pad) => ({
-        ...pad,
-        on: false,
-      }))
+      prevPads.map((pad) => {
+        return pad.id === id ? { ...pad, on: !pad.on } : pad;
+      })
     );
   }
-  const buttonElements = padsData.map((pad) => {
-    return <Pad key={pad.id} color={pad.color} on={pad.on} />;
+  const buttonElements = pads.map((pad) => {
+    return (
+      <Pad
+        id={pad.id}
+        key={pad.id}
+        color={pad.color}
+        on={pad.on}
+        toggle={toggle}
+      />
+    );
   });
   return (
     <main>
       <div className="pad-container">{buttonElements}</div>
-      <button className="all-off" onClick={turnAllPadsOff}>
-        Turn All Off{" "}
-      </button>
     </main>
   );
 }
--- React/pads/pad.jsx
+++ React/pads/pad.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 export default function Pad(props) {
-  const [on, setOn] = React.useState(props.on);
-  function toggole() {
-    setOn((prevOn) => !prevOn);
-  }
   return (
     <button
-      onClick={toggole}
-      className={on ? "on" : undefined}
+      onClick={() => props.toggle(props.id)}
+      className={props.on ? "on" : undefined}
       style={{ backgroundColor: props.color }}
     ></button>
   );
