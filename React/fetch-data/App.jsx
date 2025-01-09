890e528873adf20b73a34ce629f1176a4f0dfbce
Jie Yu <jieyu1233@gmail.com>
2025-01-08 20:12:12 -0500

    something I have forget to update on time...

--- React/fetch-data/App.jsx
+++ React/fetch-data/App.jsx
@@ -3,12 +3,19 @@ export default function App(props) {
   const [starWarsData, setstarWarsData] = React.useState(null);
   //1. get the data
   //2. save the data in state
-  fetch("https://swapi.py4e.com/api//people/1")
-    .then((res) => res.json())
-    .then((data) => setstarWarsData(data));
+  //infinite loop
+  console.log("rendered!");
+  React.useEffect(() => {
+    console.log("Effect ran");
+    fetch("https://swapi.py4e.com/api//people/1")
+      .then((res) => res.json())
+      .then((data) => setstarWarsData(data));
+  });
+
+  //side effects
   return (
     <div>
-      <pre>{JSON.stringify({ starWarsData }, null, 2)}</pre>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
 }
