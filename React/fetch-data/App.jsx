ced6c158bda2371ec0234ae95d6ff7531eaa6d1b
Jie Yu <jieyu1233@gmail.com>
2025-01-13 11:00:24 -0500

    dependencies using

--- React/fetch-data/App.jsx
+++ React/fetch-data/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 export default function App(props) {
   const [starWarsData, setstarWarsData] = React.useState(null);
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(1);
   //1. get the data
   //2. save the data in state
   //infinite loop
   console.log("rendered!");
   React.useEffect(() => {
     console.log("Effect ran");
-    fetch("https://swapi.py4e.com/api//people/1")
+    fetch(`https://swapi.py4e.com/api//people/${count}`)
       .then((res) => res.json())
       .then((data) => setstarWarsData(data));
   }, [count]);
@@ -17,7 +17,9 @@ export default function App(props) {
   return (
     <div>
       <h2>The count is {count}</h2>
-      <button onClick={() => setCount((preCount) => preCount + 1)}>Add</button>
+      <button onClick={() => setCount((preCount) => preCount + 1)}>
+        Get next character
+      </button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
