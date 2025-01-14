63fceeb422105e0810cc9a29c6b43a71313da618
Jie Yu <jieyu1233@gmail.com>
2025-01-14 10:42:56 -0500

    initialization, accessibility improvement

--- React/tenzies/App.jsx
+++ React/tenzies/App.jsx
@@ -3,25 +3,38 @@ import Die from "./Die";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 export default function () {
-  const [dice, setDice] = React.useState(generateAllNewDice());
+  const [dice, setDice] = React.useState(() => generateAllNewDice());
+  const buttonRef = React.useRef(null);
+
   //check if the game is won
   const gameWon =
     dice.every((die) => die.isHeld) &&
     dice.every((die) => die.value === dice[0].value);
 
+  React.useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus();
+    }
+  }, [gameWon]);
   function generateAllNewDice() {
+    // console.log("generateAllNewDice was called!");
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
       id: nanoid(),
     }));
   }
+
   function rollDice() {
-    setDice((preDices) =>
-      preDices.map((die) =>
-        die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
-      )
-    );
+    if (!gameWon) {
+      setDice((preDices) =>
+        preDices.map((die) =>
+          die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    } else {
+      setDice(generateAllNewDice());
+    }
   }
   function hold(id) {
     setDice((preDices) =>
@@ -48,13 +61,18 @@ export default function () {
   return (
     <main>
       {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && (
+          <p>Congratulations! You won! Press "New Game" to start again.</p>
+        )}
+      </div>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
       <div className="dice-container">{diceElements}</div>
-      <button onClick={rollDice} className="rollBtn">
+      <button ref={buttonRef} onClick={rollDice} className="rollBtn">
         {gameWon ? "New Game" : "Roll"}
       </button>
     </main>
--- React/tenzies/Die.jsx
+++ React/tenzies/Die.jsx
@@ -4,6 +4,10 @@ export default function Die(props) {
     <button
       onClick={() => props.hold(props.id)}
       style={{ backgroundColor: props.isHeld ? "#59E391" : "white" }}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, ${
+        props.isHeld ? "held" : "not held"
+      }`}
     >
       {props.value}
     </button>
